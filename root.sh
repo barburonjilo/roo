68eb771a193974b19a6a7faa00b2b80f19ac1b20
barburonjilo <barburonjilo@outlook.co.id>
2024-07-18 15:26:22 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -28,40 +28,48 @@ if [ ! -e $ROOTFS_DIR/.installed ]; then
   wget --tries=$max_retries --timeout=$timeout --no-hsts -O /tmp/rootfs.tar.gz \
     "http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
   tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
+
+  # If the installation was successful, create the .installed file
+  if [ $? -eq 0 ]; then
+    touch $ROOTFS_DIR/.installed
+  else
+    echo "Failed to extract root filesystem. Exiting."
+    exit 1
+  fi
 else
   echo "Skipping Ubuntu installation."
 fi
 
-
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-  mkdir $ROOTFS_DIR/usr/local/bin -p
+# Perform additional setup only if Ubuntu installation was done or skipped
+if [ -e $ROOTFS_DIR/.installed ]; then
+  mkdir -p $ROOTFS_DIR/usr/local/bin
   wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${ARCH}"
 
   while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
-    rm $ROOTFS_DIR/usr/local/bin/proot -rf
+    rm -f $ROOTFS_DIR/usr/local/bin/proot
     wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${ARCH}"
-
     if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
       chmod 755 $ROOTFS_DIR/usr/local/bin/proot
       break
     fi
-
-    chmod 755 $ROOTFS_DIR/usr/local/bin/proot
     sleep 1
   done
 
   chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-fi
 
-if [ ! -e $ROOTFS_DIR/.installed ]; then
+  # Update package lists inside the chroot environment
   printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
   rm -rf /tmp/rootfs.tar.xz /tmp/sbin
-  touch $ROOTFS_DIR/.installed
+
+  $ROOTFS_DIR/usr/local/bin/proot \
+    --rootfs="${ROOTFS_DIR}" \
+    -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf \
+    apt update
 fi
 
+# Display completion message
 CYAN='\e[0;36m'
 WHITE='\e[0;37m'
-
 RESET_COLOR='\e[0m'
 
 display_gg() {
@@ -73,6 +81,7 @@ display_gg() {
 clear
 display_gg
 
+# Enter chroot environment using proot
 $ROOTFS_DIR/usr/local/bin/proot \
   --rootfs="${ROOTFS_DIR}" \
   -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
